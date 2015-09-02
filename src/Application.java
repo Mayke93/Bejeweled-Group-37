cec72c7a873f443f14ea03b09bfde25c97a235ae
Mayke93 <m.l.kloppenburg@student.tudelft.nl>
2015-09-02 13:28:41 +0200

    Labels: Part ll

--- src/Application.java
+++ src/Application.java
@@ -1,7 +1,10 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.GridBagLayout;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -11,10 +14,12 @@ import javax.swing.JPanel;
 public class Application extends JFrame {
     
 	private JLabel label = new JLabel("Score: 0", JLabel.LEFT);
+	private JLabel label2 = new JLabel("Level: 1", JLabel.LEFT);
 	private JPanel leftPane = new JPanel();
 	
     public Application() {
     	label.setForeground(Color.white);
+    	label2.setForeground(Color.white);
         initUI();
         /*try {
     		setContentPane(new JLabel(new Icon(ImageIO.read(new File("test.jpg")))));
@@ -24,9 +29,16 @@ public class Application extends JFrame {
     }
 
     private void initUI() {
+    	leftPane.setLayout(new GridBagLayout());
+    	leftPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+    	
     	add(leftPane, BorderLayout.WEST);
-    	leftPane.add(label);
+    	Box box = Box.createVerticalBox();
+    	leftPane.add(box);
+    	box.add(label);
+    	box.add(label2);
     	add(new Board());
+    	
         setSize(800, 600);
         leftPane.setBackground(Color.black);
         getContentPane().setBackground(Color.black);
