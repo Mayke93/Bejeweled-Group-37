169f76d60b186b39305621a44420551c26adfdc8
Mayke93 <m.l.kloppenburg@student.tudelft.nl>
2015-09-08 12:54:13 +0200

    Proper checkstyle for CombinationTest and TileTest, empty GameTest added

---
+++ src/bejeweled37/test/GameTest.java
@@ -0,0 +1,18 @@
+package bejeweled37.test;
+
+
+import org.junit.Test;
+
+/**
+ * Simple basic test for class Game.
+ * @author Mayke Kloppenburg
+ *
+ */
+public class GameTest {
+
+  @Test
+  public void test() {
+ 
+  }
+
+}
