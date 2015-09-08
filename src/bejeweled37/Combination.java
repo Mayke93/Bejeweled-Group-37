eee6207097c22ed0eb7bde888a91a692d7eb778d
ssital <ssital@student.tudelft.nl>
2015-09-08 12:12:37 +0200

    Correct indentation

--- src/bejeweled37/Combination.java
+++ src/bejeweled37/Combination.java
@@ -4,32 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Combination {
-	public Tile.State type;
-	public List<Tile> tiles;
-	
-	public Combination(Tile.State type, List<Tile> tiles){
-		this.type = type;
-		this.tiles = tiles;
-	}
-	
-	public Combination(){
-		this.type = null;
-		this.tiles = new ArrayList<Tile>();
-	}
-	
-	public void setState(Tile.State s) {
-		this.type = s;
-	}
-	
-	public void setTiles(List<Tile> tiles) {
-		this.tiles = tiles;
-	}
-	
-	public Tile.State getState() {
-		return type;
-	}
-	
-	public List<Tile> getTiles() {
-		return tiles;
-	}
+  public Tile.State type;
+  public List<Tile> tiles;
+
+  public Combination(Tile.State type, List<Tile> tiles){
+    this.type = type;
+    this.tiles = tiles;
+  }
+
+  public Combination(){
+    this.type = null;
+    this.tiles = new ArrayList<Tile>();
+  }
+
+  public void setState(Tile.State s) {
+    this.type = s;
+  }
+
+  public void setTiles(List<Tile> tiles) {
+    this.tiles = tiles;
+  }
+
+  public Tile.State getState() {
+    return type;
+  }
+
+  public List<Tile> getTiles() {
+    return tiles;
+  }
 }
--- src/bejeweled37/Tile.java
+++ src/bejeweled37/Tile.java
@@ -8,131 +8,131 @@ import javax.swing.ImageIcon;
 
 public class Tile {
 
-    private Random random;
-    private int index;
-    private ImageIcon image;
-    private Point loc;
-    public Point d;
-    public int size;
-    private int level;
-    public boolean remove;
+  private Random random;
+  private int index;
+  private ImageIcon image;
+  private Point loc;
+  public Point d;
+  public int size;
+  private int level;
+  public boolean remove;
 
-    public static enum State{
-    	DEFAULT,NORMAL,FLAME,HYPERCUBE,STAR;
-    }
-    private State state;
+  public static enum State{
+    DEFAULT,NORMAL,FLAME,HYPERCUBE,STAR;
+  }
+  private State state;
 
-	private static final String[] paths = {"src/img/gemBlue.png", "src/img/gemGreen.png",
-							  			   "src/img/gemOrange.png", "src/img/gemPurple.png",
-							  			   "src/img/gemRed.png", "src/img/gemWhite.png",
-							  			   "src/img/gemYellow.png"};
-	public static final String[] colors = {"Blue", "Green", "Orange", "Purple",
-							  			   "Red", "White", "Yellow"};
+  private static final String[] paths = {"src/img/gemBlue.png", "src/img/gemGreen.png",
+    "src/img/gemOrange.png", "src/img/gemPurple.png",
+    "src/img/gemRed.png", "src/img/gemWhite.png",
+  "src/img/gemYellow.png"};
+  public static final String[] colors = {"Blue", "Green", "Orange", "Purple",
+    "Red", "White", "Yellow"};
 
-	
-	/**
-	 * Initalize tile with location and a random color and set state to NORMAL.
-	 * @param i location of Tile on the board
-	 * @param j location of Tile on the board
-	 */
-	public Tile(int i, int j){
-    	setRandomTile();
-    	this.state = State.NORMAL;
-    	this.loc = new Point(i,j);
-    	this.d = new Point(0,0);
-    	this.level = 0;
-    	this.remove = false;
-    	this.size = 0;
-	}
 
-	public void updateTranslation(int dx, int dy){
-		int sx = this.d.x + dx;
-		if(sx == 64) sx = 65;
-		if(sx == -64) sx = -65;
-		
-		int sy = this.d.y + dy;
-		if(sy == 64) sy = 65;
-		if(sy == -64) sy = -65;
-		this.d.setLocation(sx,sy);
-	}
-	public void resetD(){
-		this.d = new Point(0,0);
-	}
-	
-	/**
-	 * Give tile random color.
-	 */
-	public void setRandomTile(){ 
-    	this.random = new Random();
-    	this.state = State.NORMAL;
-		this.index = random.nextInt(paths.length);
-		this.image = new ImageIcon(paths[index]);
-	}
-	
-	/**
-	 * @return the Image image
-	 */
-	public Image getImage(){
-		return image.getImage();
-	}
-	
-	/**
-	 * @return the index
-	 */
-	public int getIndex(){
-		return this.index;
-	}
-	
-	/**
-	 * Returns true iff the compared with object is a tile
-	 * and has the same index, X-co and Y-co
-	 */
-	public boolean equals(Object obj){
-		if(!(obj instanceof Tile))
-			return false;
-		Tile tile = (Tile)obj;		
-		return (this.index == tile.index && tile.getX() == this.getX() && tile.getY() == this.getY());
-	}	
-	
-	/**
-	 * Returns true iff the compared with object is a Tile and has the same color.
-	 */
-	public boolean equalsColor(Object obj){
-		if(!(obj instanceof Tile))
-			return false;
-		Tile tile = (Tile)obj;		
-		return (this.index == tile.index);
-	}
+  /**
+   * Initalize tile with location and a random color and set state to NORMAL.
+   * @param i location of Tile on the board
+   * @param j location of Tile on the board
+   */
+  public Tile(int i, int j){
+    setRandomTile();
+    this.state = State.NORMAL;
+    this.loc = new Point(i,j);
+    this.d = new Point(0,0);
+    this.level = 0;
+    this.remove = false;
+    this.size = 0;
+  }
 
-	public Tile clone(int i, int j){
-		Tile t = new Tile(i,j);
-    	t.state = State.NORMAL;
-		t.index = this.index;
-		t.image = new ImageIcon(paths[index]);
-		return t;
-	}
-	
-	public int getX(){ return this.loc.x; }
-	public int getY(){ return this.loc.y; }
-	public Point getLoc(){ return this.loc;}
-	public void setLoc(Point loc){
-		this.loc = loc;
-	}
-	public void setLoc(int x, int y){
-		this.loc = new Point(x,y);
-	}
-	public int getLevel() { return level; }
-	public void setLevel(int level) { this.level = level; }
-	public void increaseLevel(){ this.level++; }
+  public void updateTranslation(int dx, int dy){
+    int sx = this.d.x + dx;
+    if(sx == 64) sx = 65;
+    if(sx == -64) sx = -65;
 
-	public State getState(){ return this.state; }
-	public void setState(State state){ this.state = state; }
-	public String toString(){
-		return "(" + Integer.toString(this.loc.x) + "," + Integer.toString(this.loc.y) + ") " + colors[this.index];
-	}
-	
-	public String getColor() {
-		String color = colors[index];
-		return color;
-	}
+    int sy = this.d.y + dy;
+    if(sy == 64) sy = 65;
+    if(sy == -64) sy = -65;
+    this.d.setLocation(sx,sy);
+  }
+  public void resetD(){
+    this.d = new Point(0,0);
+  }
+
+  /**
+   * Give tile random color.
+   */
+  public void setRandomTile(){ 
+    this.random = new Random();
+    this.state = State.NORMAL;
+    this.index = random.nextInt(paths.length);
+    this.image = new ImageIcon(paths[index]);
+  }
+
+  /**
+   * @return the Image image
+   */
+  public Image getImage(){
+    return image.getImage();
+  }
+
+  /**
+   * @return the index
+   */
+  public int getIndex(){
+    return this.index;
+  }
+
+  /**
+   * Returns true iff the compared with object is a tile
+   * and has the same index, X-co and Y-co
+   */
+  public boolean equals(Object obj){
+    if(!(obj instanceof Tile))
+      return false;
+    Tile tile = (Tile)obj;		
+    return (this.index == tile.index && tile.getX() == this.getX() && tile.getY() == this.getY());
+  }	
+
+  /**
+   * Returns true iff the compared with object is a Tile and has the same color.
+   */
+  public boolean equalsColor(Object obj){
+    if(!(obj instanceof Tile))
+      return false;
+    Tile tile = (Tile)obj;		
+    return (this.index == tile.index);
+  }
+
+  public Tile clone(int i, int j){
+    Tile t = new Tile(i,j);
+    t.state = State.NORMAL;
+    t.index = this.index;
+    t.image = new ImageIcon(paths[index]);
+    return t;
+  }
+
+  public int getX(){ return this.loc.x; }
+  public int getY(){ return this.loc.y; }
+  public Point getLoc(){ return this.loc;}
+  public void setLoc(Point loc){
+    this.loc = loc;
+  }
+  public void setLoc(int x, int y){
+    this.loc = new Point(x,y);
+  }
+  public int getLevel() { return level; }
+  public void setLevel(int level) { this.level = level; }
+  public void increaseLevel(){ this.level++; }
+
+  public State getState(){ return this.state; }
+  public void setState(State state){ this.state = state; }
+  public String toString(){
+    return "(" + Integer.toString(this.loc.x) + "," + Integer.toString(this.loc.y) + ") " + colors[this.index];
+  }
+
+  public String getColor() {
+    String color = colors[index];
+    return color;
+  }
 }
