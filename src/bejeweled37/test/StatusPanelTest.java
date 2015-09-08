df5b387febe5a75219dd927f3ac245a24084d55c
Kiran Bassi <kharkirndeepka@student.tudelft.nl>
2015-09-08 12:08:44 +0200

    StatusPanel test not complete yet

---
+++ src/bejeweled37/test/StatusPanelTest.java
@@ -0,0 +1,45 @@
+package bejeweled37.test;
+
+import static org.junit.Assert.*;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.Box;
+import javax.swing.JLabel;
+
+import org.junit.Test;
+
+public class StatusPanelTest {
+
+	private JLabel scoreLabel = new JLabel("Score:");
+	private JLabel levelLabel = new JLabel("Level:");
+
+	
+	@Test
+	public void testStatusPanel() {
+		scoreLabel.setFont(new Font("Arial", Font.BOLD, 35));
+		scoreLabel.setForeground(Color.white);
+    
+	}
+	
+	@Test
+	public void testSetScore() {
+		assertEquals("Score:", scoreLabel.getText());
+	}
+
+	
+	
+
+	@Test
+	public void testSetLevel() {
+		assertEquals("Level:", levelLabel.getText());
+	}
+	
+	
+	
+	
+	
+	
+
+}
