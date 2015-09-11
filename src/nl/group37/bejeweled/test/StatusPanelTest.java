aa55a4e0f64bb53ded1eb1ec8ad63f1b996ca2d3
Kiran Bassi <kharkirndeepka@student.tudelft.nl>
2015-09-11 17:22:33 +0200

    Checkstyle + test

--- src/nl/group37/bejeweled/test/StatusPanelTest.java
+++ src/nl/group37/bejeweled/test/StatusPanelTest.java
@@ -2,44 +2,31 @@ package nl.group37.bejeweled.test;
 
 import static org.junit.Assert.*;
 
-import java.awt.Color;
-import java.awt.Font;
-
-import javax.swing.Box;
-import javax.swing.JLabel;
-
 import org.junit.Test;
+
+import nl.group37.bejeweled.view.StatusPanel;
 
 public class StatusPanelTest {
 
-	private JLabel scoreLabel = new JLabel("Score:");
-	private JLabel levelLabel = new JLabel("Level:");
+  StatusPanel test = new StatusPanel();
 
-	
-	@Test
-	public void testStatusPanel() {
-		scoreLabel.setFont(new Font("Arial", Font.BOLD, 35));
-		scoreLabel.setForeground(Color.white);
+
+  @Test
+  public void testSetScore() {
+    test.setScore(50);
+    assertEquals("Score: 50", test.getScore());
+
+    test.setScore(100);
+    assertEquals("Score: 100", test.getScore());
+
+  }
+
+  @Test
+  public void testSetLevel() {
+
+    test.setLevel(2);
+    assertEquals("Level: 2", test.getLevel());
     
-	}
-	
-	@Test
-	public void testSetScore() {
-		assertEquals("Score:", scoreLabel.getText());
-	}
-
-	
-	
-
-	@Test
-	public void testSetLevel() {
-		assertEquals("Level:", levelLabel.getText());
-	}
-	
-	
-	
-	
-	
-	
+  }
 
 }
