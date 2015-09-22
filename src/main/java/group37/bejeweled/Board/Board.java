47c421514f7d3c79b35454cf3d83fead3917cbc7
Irene van der Blij <i.c.vanderblij@student.tudelft.nl>
2015-09-22 15:35:12 +0200

    Hashcode method for Board. <logic

--- src/main/java/group37/bejeweled/Board/Board.java
+++ src/main/java/group37/bejeweled/Board/Board.java
@@ -1,11 +1,16 @@
 package main.java.group37.bejeweled.Board;
 
+import main.java.group37.bejeweled.model.Combination;
 //import java.awt.Graphics;
 
 //import main.java.group37.bejeweled.view.Main;
 
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+
 
 /**
  * board object for the tiles.
@@ -170,8 +175,21 @@ public class Board {
     return tiles;
   }
   
-  /**
-   * method to determine if to boards are equal.
+   /** Checks whether there is a special gem in a combination.
+   * @param combi the combination to be checked
+   * @return true iff there is a special gem in the combination.
+   */
+  public boolean combinationContainsSpecialGem(Combination combi) {
+    boolean res = false;
+    for (Tile t1 : combi.getTiles()) {
+      if (t1 instanceof FlameTile || t1 instanceof HypercubeTile || t1 instanceof StarTile) {
+        res = true;
+      }
+    }
+    return res;
+  }
+    
+   /** method to determine if to boards are equal.
    * @param obj object to be compared
    * @return true iff this board is the same as object obj
    */
@@ -190,6 +208,17 @@ public class Board {
       return true;
     }
     return false; 
+  }
+  
+  /**
+   * Override the hashcode, because we also override the equals method
+   */
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + Arrays.hashCode(board);
+    return result;
   }
   
 }
