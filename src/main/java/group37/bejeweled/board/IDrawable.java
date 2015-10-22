62641751e16b5bd516557fd28e9ef277db2373f3
Irene van der Blij <i.c.vanderblij@student.tudelft.nl>
2015-10-22 10:25:00 +0200

    Revert "composite pattern implemented"
    This reverts commit ee0bd53181ce4f5f35fa35b70f30ef5652c249ef.

--- src/main/java/group37/bejeweled/board/IDrawable.java
+++
@@ -1,8 +0,0 @@
-package main.java.group37.bejeweled.board;
-
-import java.awt.Graphics;
-
-public interface IDrawable {
-  public void paintComponent(Graphics graphics, int positionX, int positionY);
-
-}
