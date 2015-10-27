3e0abf7388a77c2937fa5ae11f4773b0a7d251fa
ssital <ssital@student.tudelft.nl>
2015-10-27 17:14:26 +0100

    Fix test for specialGem methods <bug

--- src/test/java/group37/bejeweled/model/NormalCombinationTest.java
+++ src/test/java/group37/bejeweled/model/NormalCombinationTest.java
@@ -86,7 +86,7 @@ public class NormalCombinationTest {
     List<Tile> tiles = x1.getTiles();
     Tile a1 = new FlameTile(1, 4);
     tiles.add(a1);
-    assertEquals(x1.containsSpecialGem(), a1);  
+    assertEquals(x1.containsSpecialGem(), true);  
   }
   
   /**
@@ -95,7 +95,7 @@ public class NormalCombinationTest {
   @Test
   public void containsSpecialGemFalseTest() {
     Combination x1 = combinationMaker();
-    assertEquals(x1.containsSpecialGem(), null);  
+    assertEquals(x1.containsSpecialGem(), false);  
   }
   
   /**
